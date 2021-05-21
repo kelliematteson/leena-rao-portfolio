71c01dbd75b6589c35263c40218a535e824cffa2
Kellie Matteson <kelliematteson@yahoo.com>
2021-05-20 21:29:13 -0700

    added profile pic and initial customization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Leena Rao`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Leena Rao`,
+      summary: `writer extrodinaire.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
